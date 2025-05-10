4a2754d869bed4f6075fa8b169038cc8babdb16d
Obasi Peace Oluchi <132374107+DevHeart1@users.noreply.github.com>
2025-05-10 05:55:38 +0000

    feat: add reanimated-web-init file to include polyfill and Reanimated support

---
+++ utils/reanimated-web-init.ts
@@ -0,0 +1,8 @@
+// Import polyfill first
+import '@/utils/browserPolyfill';
+
+// Import Reanimated 
+import 'react-native-reanimated';
+
+// Export to ensure it's included in the bundle
+export default {};
